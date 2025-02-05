cd6490e66e74df5e6c031c1e7fa9f2ca5af181df
Ricky-Rubin <owolabi1738@gmail.com>
2025-02-05 02:13:25 +0100

    Created logic to generate diffrent color shades

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const colorBox = document.querySelector('[data-testid="colorBox"]');
+const btnOne = document.querySelector('#one');
+const btnTwo = document.querySelector('#two');
+const btnThree = document.querySelector('#three');
+const btnFour = document.querySelector('#four');
+const btnFive = document.querySelector('#five');
+const btnSix = document.querySelector('#six');
 const startGame = document.querySelector('#start');
 
 function randomColor() {
@@ -7,6 +13,13 @@ function randomColor() {
     const b = Math.floor(Math.random() * 256);
 
     colorBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`
+}
+
+function colorShade(colorValue) {
+    let percentageIncrease = Math.random() * 0.25;
+    let operandDecider = Math.random() > 0.5;
+    let newValue = operandDecider ? colorValue - colorValue * percentageIncrease : colorValue + colorValue * percentageIncrease;
+    return newValue;
 }
 
 startGame.addEventListener('click', randomColor)
